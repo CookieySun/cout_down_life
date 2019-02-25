7e31e6f24381fcfba90ecfba41967967ae43f7ad
kkt-yu <ku.k669500@gmail.com>
2019-02-25 21:51:50 +0900

    :tada: Initial Commit

---
+++ app/src/main/java/y_and_y/com/cout_down_life/MainActivity.kt
@@ -0,0 +1,12 @@
+package y_and_y.com.cout_down_life
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
