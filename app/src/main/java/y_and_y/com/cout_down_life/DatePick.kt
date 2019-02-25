4101ba50cc7c2f5b62db6e5e6b8be631afc6105f
kkt-yu <ku.k669500@gmail.com>
2019-02-25 22:38:32 +0900

    :heavy_plus_sign: create_date_picker

---
+++ app/src/main/java/y_and_y/com/cout_down_life/DatePick.kt
@@ -0,0 +1,25 @@
+package y_and_y.com.cout_down_life
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import java.util.Calendar
+
+class DatePick : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(activity!!,
+                activity as SetDate, year, month, day)
+    }
+
+    override fun onDateSet(view: android.widget.DatePicker, year: Int,
+                           monthOfYear: Int, dayOfMonth: Int) {
+    }
+
+}
